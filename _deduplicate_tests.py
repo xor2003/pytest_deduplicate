ac92d85a6c4752585c1e26129912a711c15a1549
Igor Zateryukin <igor.zateryukin@rt.ru>
2022-12-13 15:09:26 +0300

    reformat

--- _deduplicate_tests.py
+++ _deduplicate_tests.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python
 import logging
 import os
-import re
 import sys
 
 import pytest
@@ -15,7 +14,6 @@ hash_arcs = dict()
 
 
 class FindDuplicateCoverage:
-
     def __init__(self):
         self.collected = []
         self.name = None
@@ -30,29 +28,29 @@ class FindDuplicateCoverage:
 
     def pytest_runtest_logstart(self, nodeid, location):
         logging.debug(f"\nStart test {nodeid}")
-        #print(nodeid, location)
-        #file, line, name = location
+        # print(nodeid, location)
+        # file, line, name = location
         self.name = location
-        #self.name = re.sub(r'^[^/]+/', '', nodeid)
-        #print(self.name, location[2])
+        # self.name = re.sub(r'^[^/]+/', '', nodeid)
+        # print(self.name, location[2])
 
     def start_collection(self):
         try:
             self.hash = Hasher()
-            logging.debug(f"\nCoverage created")
+            logging.debug("\nCoverage created")
             self.coverage = Coverage(branch=True)
             self.coverage.start()
         except:
             self.coverage = None
 
     def pytest_report_teststatus(self, report):
-        logging.debug('pytest_report_teststatus %s' % str(report))
-        if report.when == 'setup':
+        logging.debug("pytest_report_teststatus %s" % str(report))
+        if report.when == "setup":
             self.start_collection()
-        elif report.when == 'call':
-            self.skipped = report.outcome == 'skipped'
+        elif report.when == "call":
+            self.skipped = report.outcome == "skipped"
             logging.debug(f"\nSkipped {self.skipped}")
-        elif report.when == 'teardown':
+        elif report.when == "teardown":
             self.stop_collection()
 
     def pytest_runtest_logfinish(self, nodeid, location):
@@ -62,7 +60,7 @@ class FindDuplicateCoverage:
         if self.coverage:
             try:
                 self.coverage.stop()
-                logging.debug(f"\nCoverage stopped")
+                logging.debug("\nCoverage stopped")
             except Exception as ex:
                 logging.error("Exception %s", str(ex.args()))
         if self.coverage and not self.skipped:
@@ -73,7 +71,9 @@ class FindDuplicateCoverage:
                 # logging.debug(data.measured_files())
                 myself = os.path.basename(__file__)
                 for f in data.measured_files():
-                    if os.path.basename(f) != myself and not os.path.basename(f).startswith('test_'):
+                    if os.path.basename(f) != myself and not os.path.basename(
+                        f
+                    ).startswith("test_"):
                         logging.debug(f)
                         logging.debug(data.lines(f))
                         # logging.debug(data.arcs(f))
@@ -87,14 +87,14 @@ class FindDuplicateCoverage:
                 else:
                     hash_tests[text_hash] = [self.name]
                     hash_arcs[text_hash] = arcs_list
-                logging.debug(f"\nCoverage collected")
+                logging.debug("\nCoverage collected")
 
             except Exception as ex:
                 logging.error("Exception %s", str(ex.args()))
         if self.coverage:
             try:
                 self.coverage.erase()
-                logging.debug(f"\nCoverage erased")
+                logging.debug("\nCoverage erased")
             except Exception as ex:
                 logging.error("Exception %s", str(ex.args()))
         self.name = None
@@ -108,24 +108,30 @@ my_plugin = FindDuplicateCoverage()
 pytest.main(sys.argv[1:], plugins=[my_plugin])
 
 print("Hash size: ", len(hash_tests))
-#print("Hash: ", hash_arcs)
+# print("Hash: ", hash_arcs)
 print("\n\nDuplicates:")
 for k, v in hash_tests.items():
     if len(v) > 1:
         for i in sorted(v):
             #  .trunk/trunk.yaml:7:81: [error] line too long (82 > 80 characters) (line-length)
             file, line, name = i
-            print(f"{file}:{line}:0: [warning] tests with duplicate coverage: {name} (duplicate-test)")
-        print('\n')
+            print(
+                f"{file}:{line}:0: W001 tests with duplicate coverage: {name} (duplicate-test)"
+            )
+        print("\n")
 
 print("\n\nSuperseeded:")
 for k, v in hash_tests.items():
     for kk, vv in hash_tests.items():
         if k != kk and all(ki <= kii for ki, kii in zip(hash_arcs[k], hash_arcs[kk])):
             big_file, big_line, big_name = vv[0]
-            print(f"{big_file}:{big_line}:0: [warning] test {big_name} covers more when below (bigger_coverage)")
+            print(
+                f"{big_file}:{big_line}:0: W002 test {big_name} covers more when below (bigger-coverage)"
+            )
             for i in sorted(v):
                 small_file, small_line, small_name = i
-                print(f"{small_file}:{small_line}:0: [warning] test {small_name} covers less when {big_name} (smaller_coverage)")
-                #print("%s <= %s" % (i, vv[0]))
-            print('\n')
+                print(
+                    f"{small_file}:{small_line}:0: W003 test {small_name} covers less when {big_name} (smaller-coverage)"
+                )
+                # print("%s <= %s" % (i, vv[0]))
+            print("\n")
