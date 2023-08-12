8c683ab2a40d490952958a3b711bd4dcc7403c3a
xor <xor2003@gmx.com>
2023-08-12 17:32:45 +0300

    better output

--- pytest_deduplicate.py
+++ pytest_deduplicate.py
@@ -3,13 +3,13 @@ import logging
 import os
 import sys
 
-#from line_profiler_pycharm import profile
-
 import pytest
 from _pytest.unittest import TestCaseFunction
 from coverage import Coverage
 from coverage.data import add_data_to_hash
 from coverage.misc import Hasher
+
+# from line_profiler_pycharm import profile
 
 hash_tests = {}
 hash_arcs = {}
@@ -21,31 +21,32 @@ class FindDuplicateCoverage:
         self.name = None  # the name of the current test
         self.coverage = None  # Coverage object to measure code coverage
         self.skipped = False  # flag to track if the test is skipped
-        self.coverage = Coverage(branch=True, data_file=None, omit=os.path.basename(__file__))  # initialize the Coverage object with branch coverage
-        #self.coverage = copy(self._coverage)
+        self.coverage = Coverage(branch=True, data_file=None,
+                                 omit=os.path.basename(__file__))  # initialize the Coverage object with branch coverage
+        # self.coverage = copy(self._coverage)
 
-    #@profile
+    # @profile
     def pytest_collection_modifyitems(self, items: list) -> None:
         # append test name to the collected list
         self.collected = [item.name for item in items if isinstance(item, TestCaseFunction)]
 
-    #@profile
+    # @profile
     def pytest_runtest_logstart(self, nodeid: str, location: str) -> None:
-        #logging.debug("Start test %s", nodeid)
+        # logging.debug("Start test %s", nodeid)
         self.name = location  # set the name of the current test
 
-    #@profile
+    # @profile
     def start_collection(self) -> None:
         try:
-            #logging.debug("Coverage created")
+            # logging.debug("Coverage created")
             self.coverage.erase()  # start the coverage measurement
             self.coverage.start()  # start the coverage measurement
         except:
             self.coverage = None
 
-    #@profile
+    # @profile
     def pytest_report_teststatus(self, report) -> None:
-        #logging.debug("pytest_report_teststatus %s", report)
+        # logging.debug("pytest_report_teststatus %s", report)
         if report.when == "setup":
             self.start_collection()
         elif report.when == "call":
@@ -54,11 +55,11 @@ class FindDuplicateCoverage:
         elif report.when == "teardown":
             self.stop_collection()
 
-    #@profile
+    # @profile
     def pytest_runtest_logfinish(self, nodeid, location):
         logging.debug("Stop test %s", nodeid)
 
-    #@profile
+    # @profile
     def stop_collection(self):
         if self.coverage:
             try:
@@ -70,13 +71,13 @@ class FindDuplicateCoverage:
             try:
                 data = self.coverage.get_data()
                 hasher = Hasher()  # Hasher object to hash the coverage data
-                arcs_list = []
+                arcs_list = {}
                 for file_name in data.measured_files():
                     if os.path.basename(file_name).startswith("test_"):
                         continue
                     logging.debug(file_name)
                     add_data_to_hash(data, file_name, hasher)
-                    arcs_list += [set(data.arcs(file_name))]
+                    arcs_list[file_name] = set(data.arcs(file_name))
                 if not arcs_list:
                     logging.warning("Empty arcs for %s %s", self.name, arcs_list)
                     return
@@ -96,38 +97,42 @@ class FindDuplicateCoverage:
         self.skipped = False
 
 
-#@profile
+# @profile
 def main():
     my_plugin = FindDuplicateCoverage()
     pytest.main(sys.argv[1:], plugins=[my_plugin])
 
-    #print("Hash size: ", len(hash_tests))
+    # print("Hash size: ", len(hash_tests))
+    print("\n\nSuperseeded:")
+    for coverage_hash2, tests_names2 in hash_tests.items():
+        items = []
+        for coverage_hash1, tests_names1 in hash_tests.items():
+            if coverage_hash1 != coverage_hash2 and \
+                    all(arcs2_arcs >= hash_arcs[coverage_hash1].get(arcs2_filename, set()) for
+                        arcs2_filename, arcs2_arcs in hash_arcs[coverage_hash2].items()):
+                items.extend(tests_names1)
+        if items:
+            bigger_filename, bigger_linenum, bigger_test_name = tests_names2[0]
+            print(
+                f"{bigger_filename}:{bigger_linenum}:1: I002 test {bigger_test_name} covers more code when test(s) below (bigger-coverage)",
+            )
+            for item in sorted(items):
+                smaller_filename, smaller_linenum, smaller_name = item
+                print(
+                    f"{smaller_filename}:{smaller_linenum}:1: W003 test {smaller_name} covers less code when {bigger_test_name} test (smaller-coverage)",
+                )
+            print("\n")
+
     print("\n\nDuplicates:")
     for tests_names1 in hash_tests.values():
         if len(tests_names1) == 1:
             continue
-        for i in sorted(tests_names1):
-            file, line, name = i
+        for item in sorted(tests_names1):
+            file, line, name = item
             print(
                 f"{file}:{line}:1: W001 tests with same coverage: {name} (duplicate-test)",
             )
         print("\n")
-
-    print("\n\nSuperseeded:")
-    for coverage_hash1, tests_names1 in hash_tests.items():
-        for coverage_hash2, tests_names2 in hash_tests.items():
-            if coverage_hash1 != coverage_hash2 and \
-                    all(arcs1 <= arcs2 for arcs1, arcs2 in zip(hash_arcs[coverage_hash1], hash_arcs[coverage_hash2])):
-                bigger_filename, bigger_linenum, bigger_test_name = tests_names2[0]
-                print(
-                    f"{bigger_filename}:{bigger_linenum}:1: W002 test {bigger_test_name} covers more code when test(s) below (bigger-coverage)",
-                )
-                for i in sorted(tests_names1):
-                    smaller_filename, smaller_linenum, smaller_name = i
-                    print(
-                        f"{smaller_filename}:{smaller_linenum}:1: W003 test {smaller_name} covers less code when {bigger_test_name} test (smaller-coverage)",
-                    )
-                print("\n")
 
 if __name__ == "__main__":
     main()
